ee8f675e026c0b5f06375613a8c901541a2f68c9
Nitish Kumar Singh <nitishk72@gmail.com>
2019-04-29 16:42:10 +0530

    updated readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# flutter_graphql_demo
+
+A new Flutter application with GraphQL.
+
+## Getting Started
+
+This app is using [graphql-flutter](https://github.com/zino-app/graphql-flutter#readme) Dependency. I am using version 1.0.0 of this plugin.
+
+
+Those who are on`flutter stable channel` will not able to use the version `1.0.0` of this plugin because it requires Dart Tool >= `2.2.0` && < `3.0.0` .On stable channel currently flutter is using Dart < `2.2.0` .
+
+
+To solve this issue you need to move to the dev channel of flutter which is currently using Dart >= `2.2.0`.
+
+### To change the Channel
+
+````bash
+$ flutter channel dev
+Switching to flutter channel 'dev'...
+...
+...
+..
+
+$ flutter channel
+Flutter channels:
+  beta
+* dev
+  master
+  stable
+
+```
+
+Now you can clone this app and run in your Computer.
+
+If you found any difficulties while running this app then feel free to open the issue.
+
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "GQL App",
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final HttpLink httpLink =
+        HttpLink(uri: "https://countries.trevorblades.com/");
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        link: httpLink as Link,
+        cache: OptimisticCache(
+          dataIdFromObject: typenameDataIdFromObject,
+        ),
+      ),
+    );
+    return GraphQLProvider(
+      child: HomePage(),
+      client: client,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String query = r"""
+                    query GetContinent($code : String!){
+                      continent(code:$code){
+                        name
+                        countries{
+                          name
+                        }
+                      }
+                    }
+                  """;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("GraphlQL Client"),
+      ),
+      body: Query(
+          options: QueryOptions(
+              document: query, variables: <String, dynamic>{"code": "AS"}),
+          builder: (
+            QueryResult result, {
+            VoidCallback refetch,
+          }) {
+            if (result.loading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (result.data == null) {
+              return Text("No Data Found !");
+            }
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                      result.data['continent']['countries'][index]['name']),
+                );
+              },
+              itemCount: result.data['continent']['countries'].length,
+            );
+          }),
+    );
+  }
+}
